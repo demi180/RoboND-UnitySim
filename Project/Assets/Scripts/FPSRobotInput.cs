b0b4169f700559dbbbbbcac0c82f390ba9084036
demi180 <noamweiss@gmail.com>
2017-03-06 20:33:55 -0800

    First indoor robot prototype
    Day 1 prototype with basic roomba-like robot in small indoor environment

---
+++ Project/Assets/Scripts/FPSRobotInput.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSRobotInput : MonoBehaviour
+{
+	public RobotController controller;
+
+	public bool allowStrafe;
+	public float hRotateSpeed;
+	public float vRotateSpeed;
+	public float moveSpeed;
+	public float cameraZoomSpeed;
+
+	public bool controllable;
+
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		controllable = true;
+	}
+
+	void LateUpdate ()
+	{	
+		// check if we're not focused on our robot
+		if ( controllable )
+		{
+			// check for rotation input
+			float mouseX = Input.GetAxis ( "Mouse X" );
+			float mouseY = Input.GetAxis ( "Mouse Y" );
+			controller.Rotate ( mouseX * hRotateSpeed * Time.deltaTime );
+			controller.RotateCamera ( 0, mouseY );
+
+			// check for camera zoom
+			float wheel = Input.GetAxis ( "Mouse ScrollWheel" );
+			if ( wheel != 0 )
+				controller.ZoomCamera ( -wheel * cameraZoomSpeed );
+
+			// check to reset zoom
+			if ( Input.GetMouseButtonDown ( 2 ) )
+				controller.ResetZoom ();
+
+			// check for movement input
+			if ( allowStrafe )
+			{
+				Vector3 move = new Vector3 ( Input.GetAxis ( "Horizontal" ), 0, Input.GetAxis ( "Vertical" ) ) * moveSpeed * Time.deltaTime;
+				move = controller.robotBody.TransformDirection ( move );
+				controller.Move ( move );
+				
+			} else
+			{
+				float forward = Input.GetAxis ( "Vertical" );
+				controller.Move ( forward * moveSpeed );
+			}
+
+			// check for camera switch key
+			if ( Input.GetKeyDown ( KeyCode.Tab ) )
+				controller.SwitchCamera ();
+
+			// check for unfocus input
+			if ( Input.GetKeyDown ( KeyCode.Escape ) )
+			{
+				controllable = false;
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+				return;
+			}
+		}
+		// check for focus input
+		if ( Input.GetMouseButtonDown ( 0 ) )
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			controllable = true;
+		}
+
+		// check for close app
+		if ( Input.GetKeyDown ( KeyCode.F12 ) )
+		{
+			Application.Quit ();
+		}
+	}
+}
---
+++ Project/Assets/Scripts/RobotController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+	public Transform robotBody;
+	public Transform cameraHAxis;
+	public Transform cameraVAxis;
+	public Transform fpsPosition;
+	public Transform tpsPosition;
+	public Transform actualCamera;
+	public Camera camera;
+	public CharacterController controller;
+
+	public float cameraMinAngle;
+	public float cameraMaxAngle;
+	public float cameraMinFOV;
+	public float cameraMaxFOV;
+
+	int curCamera;
+
+	void Awake ()
+	{
+		actualCamera.SetParent ( fpsPosition );
+	}
+
+	public void Move (float speed)
+	{
+		controller.SimpleMove ( robotBody.forward * speed );
+	}
+
+	public void Move (Vector3 direction)
+	{
+		direction.y = Physics.gravity.y * Time.deltaTime;
+		controller.Move ( direction );
+	}
+
+	public void Rotate (float angle)
+	{
+		robotBody.Rotate ( Vector3.up * angle );
+	}
+
+	public void RotateCamera (float horizontal, float vertical)
+	{
+//		cameraHAxis.Rotate ( Vector3.up * horizontal, Space.World );
+//		cameraVAxis.Rotate ( Vector3.right * -vertical, Space.Self );
+		Vector3 euler = cameraVAxis.localEulerAngles;
+		euler.x -= vertical;
+		if ( euler.x > 270 )
+			euler.x -= 360;
+		euler.x = Mathf.Clamp ( euler.x, cameraMinAngle, cameraMaxAngle );
+		cameraVAxis.localEulerAngles = euler;
+	}
+
+	public void ZoomCamera (float amount)
+	{
+		camera.fieldOfView += amount;
+		camera.fieldOfView = Mathf.Clamp ( camera.fieldOfView, cameraMinFOV, cameraMaxFOV );
+	}
+
+	public void ResetZoom ()
+	{
+		camera.fieldOfView = 60;
+//		camera.ResetFieldOfView ();
+	}
+
+	public void SwitchCamera ()
+	{
+		if ( curCamera == 0 )
+		{
+			curCamera = 1;
+			actualCamera.SetParent ( tpsPosition, false );
+		} else
+		{
+			curCamera = 0;
+			actualCamera.SetParent ( fpsPosition, false );
+		}
+	}
+}
