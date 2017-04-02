dbe8101e24e8b576089b9eedfa3bd4b967769bca
demi180 <noamweiss@gmail.com>
2017-04-01 19:35:52 -0700

    More work on env4

---
+++ Project/Assets/Scripts/FlyingCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingCamera : MonoBehaviour
+{
+	public float moveSpeed = 25;
+	public float rotationSpeed = 180;
+	public float sprintFactor = 4;
+
+	void Start () 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void LateUpdate ()
+	{
+		Vector3 move = new Vector3 ( Input.GetAxis ( "Horizontal" ), Input.GetAxis ( "Climb_Descend" ), Input.GetAxis ( "Vertical" ) );
+		Vector2 rotation = new Vector2 ( Input.GetAxis ( "Mouse X" ), Input.GetAxis ( "Mouse Y" ) );
+
+
+		if ( Input.GetKey ( KeyCode.LeftShift ) || Input.GetKey ( KeyCode.RightShift ) )
+			move *= sprintFactor;
+		transform.Translate ( move * moveSpeed * Time.deltaTime, Space.Self );
+		transform.Rotate ( Vector3.up * rotation.x * rotationSpeed * Time.deltaTime, Space.World );
+		transform.Rotate ( Vector3.right * -rotation.y * rotationSpeed * Time.deltaTime, Space.Self );
+
+		if ( Input.GetKeyDown ( KeyCode.Escape ) )
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+		if ( Input.GetMouseButtonDown ( 0 ) )
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+}
