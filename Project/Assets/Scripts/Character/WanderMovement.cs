9adbc9f63357118b368ecdeef039d3048be0e290
demi180 <noamweiss@gmail.com>
2017-06-02 18:42:38 -0700

    Imported some character assets & reduced size. Setup random outfits. Start on navigation and wandering

---
+++ Project/Assets/Scripts/Character/WanderMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderMovement : MonoBehaviour
+{
+	public NavMeshAgent agent;
+
+	Transform myTransform;
+	float start;
+
+	void Awake ()
+	{
+		myTransform = GetComponent<Transform> ();
+		agent.autoBraking = false;
+
+		start = Random.value * 1000f;
+		float y = Random.value * 360f;
+		Vector3 euler = transform.eulerAngles;
+		euler.y = y;
+		transform.eulerAngles = euler;
+	}
+
+	void LateUpdate ()
+	{
+		Vector3 euler = transform.eulerAngles;
+		euler.y += 0.5f - Mathf.PerlinNoise ( start + Time.time, start + Time.time );
+		transform.eulerAngles = euler;
+		agent.Move ( transform.forward * agent.speed * Time.deltaTime );
+
+		return;
+//		Vector3 forward = Vector3.forward;
+		Vector3 forward = transform.forward;
+		forward.x = 0.5f - Mathf.PerlinNoise ( start + Time.time, 0 )/3;
+		forward.z = 0.5f - Mathf.PerlinNoise ( 0, start + Time.time )/3;
+//		Debug.Log ( "1\t\t" + Mathf.PerlinNoise ( start + Time.time, 0 ) );
+//		Debug.Log ( "2\t\t" + Mathf.PerlinNoise ( 0, start + Time.time ) );
+//		Debug.Log ( forward.normalized );
+		transform.rotation = Quaternion.LookRotation ( forward.normalized, Vector3.up );
+		agent.Move ( forward.normalized * agent.speed * Time.deltaTime );
+	}
+}
